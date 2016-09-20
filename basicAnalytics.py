34954db2d0429c042ef23659acedcaa3c185c330
Matt <mjsmith11@my.trine.edu>
2016-09-19 22:47:38 -0400

    Add Pychharm files add count of skipped records remove headers from data work on basic analytics

--- basicAnalytics.py
+++ basicAnalytics.py
@@ -1,12 +1,80 @@
 import utilities as u
+import numpy as np
 
-def frequencies(data,field):
-	result = {}
-	for row in data:
-		value = row[u.indexLookup(field)]
-		if (result.has_key(value)):
-			result[value] = result[value]+1
-		else:
-			result[value] = 1
-	
-	return result
+
+# accepts a data list created by ingestion.py and field name as a string
+# returns a dictionary with data values from the provided field in the provided data as keys and number of occurances as values
+def frequencies(data, field):
+    result = {}
+    for row in data:
+        value = row[u.indexLookup(field)]
+        if (result.has_key(value)):
+            result[value] = result[value] + 1
+        else:
+            result[value] = 1
+
+    return result
+
+
+# accepts a data list created by ingestion.py and field name as a string
+# returns a dictionary with 2 entries. The numpy array with key 'array' and the number of missing elements with key 'missing elements'
+def getNumpyArray(data, field):
+    mylist = []
+    missingElements = 0
+    fieldNum = u.indexLookup(field)
+
+    for row in data:
+        if row[fieldNum] == "":
+            missingElements += 1
+        else:
+            mylist.append(row[fieldNum])
+
+    return {'array':np.array(mylist), 'missing elements':missingElements}
+
+#accepts an array of numbers as a parameter
+#returns an dictionary with element 'array' containing the array with outliers removed and element 'outliers' containing a list of removed values
+#values that are not within 3 standard deviations of the mean are considered outliers
+def removeOutliers(array):
+    mean = np.mean(array)
+    stddev = np.std(array)
+    upperbound = mean+3*stddev
+    lowerbound = mean-3*stddev
+    removeIndices = []
+    removedValues = []
+    for i,num in enumerate(array):
+        if num>upperbound or num<lowerbound:
+            removeIndices.append(i)
+            removedValues.append(num)
+    array = np.delete(array,removeIndices)
+    return {'array':array,'outliers':removedValues}
+
+#returns a dictionary with the following analytics computed for the passed array
+#   'n'                 : number of data values including outliers
+#   'number of outliers : number of outliers that were removed
+#   'outliers'          : a list of outliers that were removed
+#   'max'               : max of the data excluding outliers
+#   'min'               : min of the data excluding outliers
+#   'mean'              : mean of the data excluding outliers
+#   'median'            : median of the data excluding outliers
+#   'Q1'                : the first quartile of the data excluding outliers
+#   'Q3'                : the third quartile of the data excluding outliers
+#   'stddev'            : standard deviation of the data excluding outliers
+def computeBasicNumericalAnalytics(array):
+    result = {}
+    result['n'] = len(array)
+    outlierDict = removeOutliers(array)
+    noOutlierArr = outlierDict['array']
+    result["number of outliers"] = len(outlierDict['outliers'])
+    result['outliers'] = outlierDict['outliers']
+    result['max'] = np.amax(noOutlierArr)
+    result['min'] = np.amin(noOutlierArr)
+    result['mean'] = np.mean(noOutlierArr)
+    result['median'] = np.median(noOutlierArr)
+    result['Q1'] = np.percentile(noOutlierArr,25)
+    result['Q3'] = np.percentile(noOutlierArr,75)
+    result['stddev'] = np.std(noOutlierArr)
+
+    return result
+
+
+
--- ingestion.py
+++ ingestion.py
@@ -3,86 +3,96 @@ import utilities as u
 
 data = []
 
+
+# accepts a file path as a string and parses it as csv
+# each element of the list data will be a list of length 75 with entries ordered according to the index lookup in utilities.py
+# returns an integer indicating the number of records removed due to improper formatting
 def readAndPreprocess(file):
-	f = open(file)
-	csv_f = csv.reader(f)
-	for row in csv_f:
-		#length other than 75 indicates an error in the data object
-		if (len(row) == 75):
-			row[0]  = u.checkNotBlank(row[0])		#unique_transaction_id
-			row[1]  = u.checkDollars(row[1])		#dollarsobligated
-			row[2]  = u.checkDollars(row[2])		#baseandexercisedoptionsvalue
-			row[3]  = u.checkDollars(row[3])		#baseandalloptionsvalue
-			row[4]  = u.checkNotBlank(row[4])		#contractingofficeagencyid
-			row[5]  = u.checkNotBlank(row[5])		#contractingofficeid
-			row[6]  = u.checkNotBlank(row[6])		#fundingrequestingagencyid
-			row[7]  = u.checkNotBlank(row[7])		#fundingrequestingofficeid
-			row[8]  = u.checkNotBlank(row[8])		#fundedbyforeignentity
-			row[9]  = u.checkDate(row[9])			#signeddate
-			row[10] = u.checkDate(row[10])			#effectivedate
-			row[11] = u.checkDate(row[11])			#currentcompletiondate
-			row[12] = u.checkNotBlank(row[12])		#typeofcontractpricing
-			row[13] = u.checkNotBlank(row[13]) 		#priceevaluationpercentdifference
-			row[14] = u.checkNotBlank(row[14])		#subcontractplan
-			row[15] = u.checkYesNo(row[15])			#multiyearcontract
-			row[16] = u.checkNotBlank(row[16])		#majorprogramcode
-			row[17] = u.checkNotBlank(row[17])		#contractfinancing
-			row[18] = u.checkNotBlank(row[18])		#descriptionofcontractrequirement
-			row[19] = u.checkNotBlank(row[19])		#nationalinterestactioncode
-			row[20] = u.checkNotBlank(row[20])		#vendorname
-			row[21] = u.checkNotBlank(row[21])		#vendoralternatename
-			row[22] = u.checkNotBlank(row[22])		#vendorlegalorganizationname
-			row[23] = u.checkNotBlank(row[23])		#vendordoingasbusinessname
-			row[24] = u.checkNotBlank(row[24])		#divisionname
-			row[25] = u.checkNotBlank(row[25])		#divisionnumberorofficecode
-			row[26] = u.checkNotBlank(row[26])		#streetaddress
-			row[27] = u.checkNotBlank(row[27])		#streetaddress2
-			row[28] = u.checkNotBlank(row[28])		#streetaddress3
-			row[29] = u.checkNotBlank(row[29])		#city
-			row[30] = u.checkNotBlank(row[30])		#state
-			row[31] = u.checkInt(row[31])			#zipcode
-			row[32] = u.checkNotBlank(row[32])		#vendorcountrycode
-			row[33] = u.checkNotBlank(row[33])		#vendor_state_code
-			row[34] = u.checkInt(row[34])			#vendor_cd
-			row[35] = u.checkInt(row[35])			#congressionaldistrict
-			row[36] = u.checkInt(row[36])			#phoneno
-			row[37] = u.checkNotBlank(row[37])		#statecode
-			row[38] = u.checkNotBlank(row[38])		#placeofperformancecity
-			row[39] = u.checkNotBlank(row[39])		#pop_state_code
-			row[40] = u.checkNotBlank(row[40])		#placeofperformancecountrycode
-			row[41] = u.checkInt(row[41])			#placeofperformancezipcode
-			row[42] = u.checkNotBlank(row[42])		#pop_cd
-			row[43] = u.checkNotBlank(row[43])		#placeofperformancecongressionaldistrict
-			row[44] = u.checkNotBlank(row[44])		#informationtechnologycommercialitemcategory
-			row[45] = u.checkNotBlank(row[45])		#useofepadesignatedproducts
-			row[46] = u.checkNotBlank(row[46])		#seatransportation
-			row[47] = u.checkNotBlank(row[47])		#countryoforigin
-			row[48] = u.checkNotBlank(row[48])		#placeofmanufacture
-			row[49] = u.checkNotBlank(row[49])		#agencyid
-			row[50] = u.checkInt(row[50])			#numberofoffersreceived
-			row[51] = u.checkNotBlank(row[51])		#organizationaltype
-			row[52] = u.checkInt(row[52])			#numberofemployees
-			row[53] = u.checkInt(row[53])			#annualRevenue
-			row[54] = u.checkTF(row[54])			#hbcuflag
-			row[55] = u.checkTF(row[55])			#educationalinstitutionflag
-			row[56] = u.checkTF(row[56])			#womanownedflag
-			row[57] = u.checkTF(row[57])			#veteranownedflag
-			row[58] = u.checkTF(row[58])			#localgovernmentflag
-			row[59] = u.checkTF(row[59])			#minorityinstitutionflag
-			row[60] = u.checkTF(row[60])			#aiobflag
-			row[61] = u.checkTF(row[61])			#stategovernmentflag
-			row[62] = u.checkTF(row[62])			#federalgovernmentflag
-			row[63] = u.checkTF(row[63])			#minorityownedbusinessflag
-			row[64] = u.checkTF(row[64])			#apaobflag
-			row[65] = u.checkTF(row[65])			#tribalgovernmentflag
-			row[66] = u.checkTF(row[66])			#baobflag
-			row[67] = u.checkTF(row[67])			#naobflag
-			row[68] = u.checkTF(row[68])			#saaobflag
-			row[69] = u.checkTF(row[69])			#nonprofitorganizationflag
-			row[70] = u.checkTF(row[70])			#isfoundation
-			row[71] = u.checkTF(row[71])			#haobflag
-			row[72] = u.checkTF(row[72])			#isprivateuniversityorcollege
-			row[73] = u.checkTF(row[73])			#isstatecontrolledinstitutionofhigherlearning
-			row[74] = u.checkDate(row[74])			#last_modified_date
-			
-			data.append(row)
+    skippedRecords = 0
+    f = open(file)
+    csv_f = csv.reader(f)
+    for row in csv_f:
+        # length other than 75 indicates an error in the data object
+        if (len(row) == 75):
+            row[0] = u.checkNotBlank(row[0])  # unique_transaction_id
+            row[1] = u.checkDollars(row[1])  # dollarsobligated
+            row[2] = u.checkDollars(row[2])  # baseandexercisedoptionsvalue
+            row[3] = u.checkDollars(row[3])  # baseandalloptionsvalue
+            row[4] = u.checkNotBlank(row[4])  # contractingofficeagencyid
+            row[5] = u.checkNotBlank(row[5])  # contractingofficeid
+            row[6] = u.checkNotBlank(row[6])  # fundingrequestingagencyid
+            row[7] = u.checkNotBlank(row[7])  # fundingrequestingofficeid
+            row[8] = u.checkNotBlank(row[8])  # fundedbyforeignentity
+            row[9] = u.checkDate(row[9])  # signeddate
+            row[10] = u.checkDate(row[10])  # effectivedate
+            row[11] = u.checkDate(row[11])  # currentcompletiondate
+            row[12] = u.checkNotBlank(row[12])  # typeofcontractpricing
+            row[13] = u.checkNotBlank(row[13])  # priceevaluationpercentdifference
+            row[14] = u.checkNotBlank(row[14])  # subcontractplan
+            row[15] = u.checkYesNo(row[15])  # multiyearcontract
+            row[16] = u.checkNotBlank(row[16])  # majorprogramcode
+            row[17] = u.checkNotBlank(row[17])  # contractfinancing
+            row[18] = u.checkNotBlank(row[18])  # descriptionofcontractrequirement
+            row[19] = u.checkNotBlank(row[19])  # nationalinterestactioncode
+            row[20] = u.checkNotBlank(row[20])  # vendorname
+            row[21] = u.checkNotBlank(row[21])  # vendoralternatename
+            row[22] = u.checkNotBlank(row[22])  # vendorlegalorganizationname
+            row[23] = u.checkNotBlank(row[23])  # vendordoingasbusinessname
+            row[24] = u.checkNotBlank(row[24])  # divisionname
+            row[25] = u.checkNotBlank(row[25])  # divisionnumberorofficecode
+            row[26] = u.checkNotBlank(row[26])  # streetaddress
+            row[27] = u.checkNotBlank(row[27])  # streetaddress2
+            row[28] = u.checkNotBlank(row[28])  # streetaddress3
+            row[29] = u.checkNotBlank(row[29])  # city
+            row[30] = u.checkNotBlank(row[30])  # state
+            row[31] = u.checkInt(row[31])  # zipcode
+            row[32] = u.checkNotBlank(row[32])  # vendorcountrycode
+            row[33] = u.checkNotBlank(row[33])  # vendor_state_code
+            row[34] = u.checkInt(row[34])  # vendor_cd
+            row[35] = u.checkInt(row[35])  # congressionaldistrict
+            row[36] = u.checkInt(row[36])  # phoneno
+            row[37] = u.checkNotBlank(row[37])  # statecode
+            row[38] = u.checkNotBlank(row[38])  # placeofperformancecity
+            row[39] = u.checkNotBlank(row[39])  # pop_state_code
+            row[40] = u.checkNotBlank(row[40])  # placeofperformancecountrycode
+            row[41] = u.checkInt(row[41])  # placeofperformancezipcode
+            row[42] = u.checkNotBlank(row[42])  # pop_cd
+            row[43] = u.checkNotBlank(row[43])  # placeofperformancecongressionaldistrict
+            row[44] = u.checkNotBlank(row[44])  # informationtechnologycommercialitemcategory
+            row[45] = u.checkNotBlank(row[45])  # useofepadesignatedproducts
+            row[46] = u.checkNotBlank(row[46])  # seatransportation
+            row[47] = u.checkNotBlank(row[47])  # countryoforigin
+            row[48] = u.checkNotBlank(row[48])  # placeofmanufacture
+            row[49] = u.checkNotBlank(row[49])  # agencyid
+            row[50] = u.checkInt(row[50])  # numberofoffersreceived
+            row[51] = u.checkNotBlank(row[51])  # organizationaltype
+            row[52] = u.checkInt(row[52])  # numberofemployees
+            row[53] = u.checkInt(row[53])  # annualRevenue
+            row[54] = u.checkTF(row[54])  # hbcuflag
+            row[55] = u.checkTF(row[55])  # educationalinstitutionflag
+            row[56] = u.checkTF(row[56])  # womanownedflag
+            row[57] = u.checkTF(row[57])  # veteranownedflag
+            row[58] = u.checkTF(row[58])  # localgovernmentflag
+            row[59] = u.checkTF(row[59])  # minorityinstitutionflag
+            row[60] = u.checkTF(row[60])  # aiobflag
+            row[61] = u.checkTF(row[61])  # stategovernmentflag
+            row[62] = u.checkTF(row[62])  # federalgovernmentflag
+            row[63] = u.checkTF(row[63])  # minorityownedbusinessflag
+            row[64] = u.checkTF(row[64])  # apaobflag
+            row[65] = u.checkTF(row[65])  # tribalgovernmentflag
+            row[66] = u.checkTF(row[66])  # baobflag
+            row[67] = u.checkTF(row[67])  # naobflag
+            row[68] = u.checkTF(row[68])  # saaobflag
+            row[69] = u.checkTF(row[69])  # nonprofitorganizationflag
+            row[70] = u.checkTF(row[70])  # isfoundation
+            row[71] = u.checkTF(row[71])  # haobflag
+            row[72] = u.checkTF(row[72])  # isprivateuniversityorcollege
+            row[73] = u.checkTF(row[73])  # isstatecontrolledinstitutionofhigherlearning
+            row[74] = u.checkDate(row[74])  # last_modified_date
+
+            data.append(row)
+
+        else:
+            skippedRecords += 1
+    del data[0]  # this is the headings
+    return skippedRecords
