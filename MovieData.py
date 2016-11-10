510a3413a39ffc00be9071595e259a953381a143
Matt <mjsmith11@my.trine.edu>
2016-11-09 22:08:34 -0500

    Divide out title and year

--- MovieData.py
+++ MovieData.py
@@ -5,6 +5,7 @@
 from DataFrameBase import DataFrameBaseType
 import datetime
 import pandas as pd
+import re
 
 class MovieData(DataFrameBaseType):
     def __init__(self):
@@ -19,9 +20,22 @@ class MovieData(DataFrameBaseType):
         self.ratings = pd.read_table(dirpath+'\\ratings.dat', sep='::', header=None, names=rnames, engine='python')
         self.ratings['datetime'] = self.ratings['timestamp'].map(lambda x: datetime.datetime.fromtimestamp(x))
         self.ratings.drop('timestamp',axis=1,inplace=True)
-        mnames = ['movie_id', 'title', 'genres']
+        mnames = ['movie_id', 'titleandyear', 'genres']
         self.movies = pd.read_table(dirpath+'\\movies.dat', sep='::', header=None,names=mnames,engine='python')
-        print self.ratings
+        self.movies['year']=self.movies['titleandyear'].map(lambda mov: self.getMovieYear(mov))
+        self.movies['title'] = self.movies['titleandyear'].map(lambda mov: self.getMovieName(mov))
+        self.movies.drop('titleandyear',axis=1,inplace=True)
+
+
+    def getMovieYear(self,movieStr):
+        searchRes = re.search(r"\(\d\d\d\d\)",movieStr)
+        return searchRes.group(0)[1:len(searchRes.group(0))-1] # remove parentheses
+
+    def getMovieName(self,movieStr):
+        year = self.getMovieYear(movieStr)
+        year = "("+year+")"
+        return movieStr[0:movieStr.index(year)-1]
+
 
 
 
