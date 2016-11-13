e03079131af93a54780304fe7fd8ce8aa8fab413
Matt <mjsmith11@my.trine.edu>
2016-11-13 13:34:14 -0500

    Add function comments and reorganize

--- MovieData.py
+++ MovieData.py
@@ -17,6 +17,10 @@ class MovieData(DataFrameBaseType):
         self.ratings = ''
         self.movies = ''
 
+    #dirpath should be a path to a directory containing users.dat, ratings.dat, and movies.dat without a trailing \
+    #this method reads the files and creates a datafram for each file self.users, self.ratings, and self.movies
+    #unix timestamps are converted to datetime objects and years are separated from titles.
+    #self.df is the result of merging the three dataframes
     def loadMovieData(self,dirpath):
         unames = ['user_id', 'gender', 'age', 'occupation', 'zip']
         self.users = pd.read_table(dirpath+'\\users.dat', sep='::', header=None, names=unames, engine='python')
@@ -33,16 +37,18 @@ class MovieData(DataFrameBaseType):
         self.df = pd.merge(tempdf,self.movies,on='movie_id',how='inner')
 
 
-
+    #returns the year as yyyy from the "movie (year)" string
     def getMovieYear(self,movieStr):
         searchRes = re.search(r"\(\d\d\d\d\)",movieStr)
         return searchRes.group(0)[1:len(searchRes.group(0))-1] # remove parentheses
 
+    #returns the movie title from the "movie (year)" string
     def getMovieName(self,movieStr):
         year = self.getMovieYear(movieStr)
         year = "("+year+")"
         return movieStr[0:movieStr.index(year)-1]
 
+    #returns a string corresponding to the passed occupation code
     def getOccupationName(self, code):
         dict = {
             0:"other or not specified",
@@ -69,6 +75,7 @@ class MovieData(DataFrameBaseType):
         }
         return dict[code]
 
+    #returns a string corresponding to the passed age range code
     def getAgeRange(self,code):
         dict = {
             1:"Under 18",
@@ -97,6 +104,8 @@ class MovieData(DataFrameBaseType):
 
     #basic analytics
 
+    #returns a series with years as indices and movies released that year for values
+    #shows a barchart visualization if visualize is passed as true
     def releaseYearDistribution(self,visualize):
         freq = self.movies['year'].value_counts(sort=False)
         freq.sort_index(inplace=True)
@@ -108,17 +117,12 @@ class MovieData(DataFrameBaseType):
             plot.show()
         return freq
 
+    #returns a series with up to limit movies that have the most ratings
     def mostRatedMovies(self,limit):
         return self.filteredTopNFreq(r".*",limit,"title")
 
-    def ratingsEachDayOfWeek(self):
-        self.df['weekday']=self.df['datetime'].map(lambda dt: u.getDayStr(dt.weekday()))
-        grouped = self.df['rating'].groupby(self.df['weekday'])
-        avgRatings = grouped.mean()
-        avgRatings.sort_values(inplace=True,ascending=False)
-        self.df.drop('weekday',axis=1,inplace=True)
-        return avgRatings
 
+    #returns a series with indexes ['min','max','mean','standard deviation','25%','median','75%'] and values representing these statistics for time in years between movie release and reviews
     def ReleaseToReviewYears(self):
         self.df['deltaT'] = self.df['datetime'].map(lambda dt: dt.year) - self.df['year'].astype(int)
         dataList = []
@@ -133,6 +137,8 @@ class MovieData(DataFrameBaseType):
         self.df.drop('deltaT',axis=1,inplace=True)
         return Series(dataList,index=['min','max','mean','standard deviation','25%','median','75%'])
 
+    #returns a series indexed by occupation strings and contains values that are python lists of the most frequently reviewed movie by that occupation
+    #the list is required for bimodal data
     def mostRatedByOccupation(self):
         grouped = self.df['title'].groupby(self.df['occupation'])
         dict = {}
@@ -140,13 +146,29 @@ class MovieData(DataFrameBaseType):
             dict[self.getOccupationName(code)] = group.mode().tolist()
         return Series(dict)
 
+    #returns a series indexed by age group strings with values of the most common hour for that age group
+    #values are python list objects in order to accomdate bimodal data
+    def mostPopularHourByAgeGroup(self):
+        self.df['hour']=self.df['datetime'].map(lambda dt: dt.hour)
+        grouped = self.df['hour'].groupby(self.df['age'])
+        dict = {}
+        for age,group in grouped:
+            dict[self.getAgeRange(age)]=group.astype(int).mode().tolist()
+        self.df.drop('hour',axis=1,inplace=True)
+        return Series(dict)
+
     #advanced analytics
+
+    #returns a series indexed by zipcodes with the +4 digits removed for US zips and values counting reviews from that zip
     def distinctZipCodesAndCounts(self):
         self.df["short_zip"]=self.df['zip'].map(lambda z : self.removeZipPlus4(z))
         res = self.frequencies('short_zip')
         self.df.drop("short_zip",axis=1,inplace=True)
         return res
 
+
+    #returns a python dictionary of stats calculated on each movie's average rating
+    #keys are max,max_titles,min,min_titles,mean,std,25%,50%,75%
     def ratingStats(self,visualize):
         result = {}
         grouped = self.df['rating'].groupby(self.df['title'])
@@ -186,6 +208,7 @@ class MovieData(DataFrameBaseType):
 
         return result
 
+    #this returns a series indexed by genre names with average ratings for that genre as values
     def bestAndWorstGenres(self):
         genreDummies = self.df['genres'].str.get_dummies() # this is a dataframe indexed the same as self.df and each column is a genre. values are 1 if that index has the genre and 0 if not
         for column in genreDummies:
@@ -195,6 +218,7 @@ class MovieData(DataFrameBaseType):
 
         return means
 
+    #returns a series indexed by genre and values are names of occupations that reviewd that genre most
     def occupationGenrePreference(self):
         genreDummies = self.df['genres'].str.get_dummies()  # this is a dataframe indexed the same as self.df and each column is a genre. values are 1 if that index has the genre and 0 if not
         for column in genreDummies:
@@ -202,11 +226,12 @@ class MovieData(DataFrameBaseType):
         genreDummies = genreDummies.replace(0, np.NaN)
         return genreDummies.mode(axis=0).ix[0].map(lambda x: self.getOccupationName(x))
 
-    def mostPopularHourByAgeGroup(self):
-        self.df['hour']=self.df['datetime'].map(lambda dt: dt.hour)
-        grouped = self.df['hour'].groupby(self.df['age'])
-        dict = {}
-        for age,group in grouped:
-            dict[self.getAgeRange(age)]=group.astype(int).mode().tolist()
-        self.df.drop('hour',axis=1,inplace=True)
-        return Series(dict)
+
+    #teturns a series indexed by day of the week and values representing the average rating given on that day
+    def ratingsEachDayOfWeek(self):
+        self.df['weekday']=self.df['datetime'].map(lambda dt: u.getDayStr(dt.weekday()))
+        grouped = self.df['rating'].groupby(self.df['weekday'])
+        avgRatings = grouped.mean()
+        avgRatings.sort_values(inplace=True,ascending=False)
+        self.df.drop('weekday',axis=1,inplace=True)
+        return avgRatings
